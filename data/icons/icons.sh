64f683b87f77958ac06b93e3dea2e0d06827ecb6
Timecraft <timemaster23x@gmail.com>
2018-06-26 20:17:59 -0400

    Easy manual install of icons

---
+++ data/icons/icons.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo cp 16/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/16x16/apps/
+
+sudo cp 32/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/32x32/apps/
+
+sudo cp 64/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/64x64/apps/
+
+sudo cp 128/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/128x128/apps/
