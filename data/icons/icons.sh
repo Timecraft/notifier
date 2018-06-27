ae13082afc0b499e963010e8c74b0a6402316b5c
Timecraft <timemaster23x@gmail.com>
2018-06-26 20:19:45 -0400

    Echo's installed for feedback

--- data/icons/icons.sh
+++ data/icons/icons.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 sudo cp 16/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/16x16/apps/
+echo Installed 16x16
 
 sudo cp 32/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/32x32/apps/
+echo Installed 32x32
 
 sudo cp 64/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/64x64/apps/
+echo Installed 64x64
 
 sudo cp 128/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/128x128/apps/
+echo Installed 128x128
