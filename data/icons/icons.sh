0eb411f33c320c731395acb8bdb535cec02b9c1b
Timecraft <timemaster23x@gmail.com>
2018-06-29 20:00:46 -0400

    There's an x256 as well

--- data/icons/icons.sh
+++ data/icons/icons.sh
@@ -11,3 +11,6 @@ echo Installed 64x64
 
 sudo cp 128/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/128x128/apps/
 echo Installed 128x128
+
+sudo cp 256/com.github.timecraft.notifier.svg /usr/share/icons/hicolor/256x256/apps/
+echo Installed 256x256
